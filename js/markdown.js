b2ea2865f3e21fc56817dffd7dcd57c1e773bf7d
mifongjvav <smmomm@126.com>
2025-12-20 11:32:43 +0800

    更新

--- js/markdown.js
+++ js/markdown.js
@@ -12,6 +12,8 @@
 
 想要提交自己的函数？[<i class="fas fa-code-pull-request"></i>为AFS仓库提交拉取请求</i>](https://github.com/mifongjvav/AFS/compare) 或 让我来：QQ 172013661
 
+想要参与网站开发？[<i class="fa-brands fa-github"></i>AFS-Web的Github仓库](https://github.com/mifongjvav/AFS-Web/)
+
 > **提示**：切换主题请将鼠标放在 **<i class="fas fa-brush"></i>** 按钮上
 
         `;
